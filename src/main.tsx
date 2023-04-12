5f705f6e6fe4bf1b7c5e80eeb4b2e0ac764dac6c
linxubin <905637031@qq.com>
2023-04-12 15:10:49 +0800

    feat: 相关初始化

---
+++ src/main.tsx
@@ -0,0 +1,8 @@
+import ReactDOM from 'react-dom/client'
+import App from './App'
+
+import '@assets/js/common.js'
+
+import '@assets/style/reset.css'
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(<App />)
